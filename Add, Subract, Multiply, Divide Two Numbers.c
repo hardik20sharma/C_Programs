c4c52c5b4a1fdfc173d172890ee7f299fc2aa35c
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-09-26 12:08:07 +0530

    Update Add, Subract, Multiply, Divide Two Numbers.c

--- Add, Subract, Multiply, Divide Two Numbers.c
+++ Add, Subract, Multiply, Divide Two Numbers.c
@@ -6,12 +6,8 @@ void main()
 	float b, c;
 	printf("Enter number 1: "); scanf("%d",&a);
 	printf("Enter number 2: "); scanf("%f",&b);
-	c=a+b;
-	printf("The sum is %f",c);
-	c=a-b;
-	printf("\nThe difference is %f",c);
-	c=a*b;
-	printf("\nThe product is %f",c);
-	c=a/b;
-	printf("\nThe answer on division is %f",c);
+	c=a+b; printf("The sum is %f",c);
+	c=a-b; printf("\nThe difference is %f",c);
+	c=a*b; printf("\nThe product is %f",c);
+	c=a/b; printf("\nThe answer on division is %f",c);
 }
