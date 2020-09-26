9c7498ba961e2fdf36922d8b0d269554cef9f2b3
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-09-26 12:11:33 +0530

    Update Celsius To Fahrenehit.c

--- Celsius To Fahrenehit.c
+++ Celsius To Fahrenehit.c
@@ -4,6 +4,6 @@ void main()
 {
 	float ctemp, ftemp;
 	printf("Enter the temperature in Celsius: "); scanf("%f", &ctemp);
-	ftemp=(1.8*ctemp)+32;
+	ftemp = (1.8 * ctemp) + 32;
 	printf("The temperature in Fahrenheit is %f", ftemp);
 }
