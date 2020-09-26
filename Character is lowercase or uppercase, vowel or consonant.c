a80414976dd9d07d7c8b2d6e19181a8d79c463c2
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-09-26 12:12:33 +0530

    Update Character is lowercase or uppercase, vowel or consonant.c

--- Character is lowercase or uppercase, vowel or consonant.c
+++ Character is lowercase or uppercase, vowel or consonant.c
@@ -11,11 +11,11 @@ void main()
 	else
 	{
 		printf("The character is in uppercase");
-		c=tolower(c);
+		c = tolower(c);
 	}
 	
 	printf("\nThe character is ");
-	if(c=='a' || c=='e'|| c=='i' || c=='o' || c=='u')
+	if(c == 'a' || c == 'e'|| c == 'i' || c == 'o' || c == 'u')
 		printf("a vowel");
 	else
 		printf("a consonant");
