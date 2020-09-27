1da2a8886f1eba3e1eadbed061b59a1ed51e9083
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-09-27 11:14:28 +0530

    Update Exchanging Two Numbers.c

--- Exchanging Two Numbers.c
+++ Exchanging Two Numbers.c
@@ -5,9 +5,9 @@ void main()
 	int x, y, temp;
 	printf("Enter the value of x: "); scanf("%d",&x);
 	printf("Enter the value of y: "); scanf("%d",&y);
-	temp=x;
-	x=y;
-	y=temp;
-	printf("Value of x now is %d",x);
-	printf("\nValue of y now is %d",y);
+	temp = x;
+	x = y;
+	y = temp;
+	printf("Value of x now is %d", x);
+	printf("\nValue of y now is %d", y);
 }
