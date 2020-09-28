8be43243bcf551345fda6b40c48c2992006830ec
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-09-28 18:44:44 +0530

    Update Increament Decreament.c

--- Increament Decreament.c
+++ Increament Decreament.c
@@ -3,17 +3,16 @@
 void main()
 {
 	int x, y;
-	
+
 	printf("Enter the value of x: "); scanf("%d",&x);
 	printf("Enter the value of y: "); scanf("%d",&y);
-	
+
 	printf("\nValue after ++x: %d",++x);
 	printf("\nValue after --y: %d",--y);
-	
+
 	x++;
 	y--;
-	
+
 	printf("\nValue after x++: %d",x);
 	printf("\nValue after y--: %d",y);
-
 }
