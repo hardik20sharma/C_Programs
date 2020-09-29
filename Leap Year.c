4f89f03431fe7ceedf233af29dcd52e6bf72b4a1
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-09-29 18:21:29 +0530

    Update Leap Year.c

--- Leap Year.c
+++ Leap Year.c
@@ -3,13 +3,14 @@
 void main()
 {
 	int year;
-	printf("Enter the year: "); scanf("%d", &year);
+	printf("Enter the year: ");
+	scanf("%d", &year);
 	
-	if(year%4!=0)
+	if(year % 4 != 0)
 		printf("No");
-	else if(year%100!=0)
+	else if(year % 100 != 0)
 		printf("Yes");
-	else if(year%400!=0)
+	else if(year % 400 != 0)
 		printf("No");
 	else
 		printf("Yes");	
