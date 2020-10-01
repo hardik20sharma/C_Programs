783a1286c6af285e0976b1af2363239e829e0e74
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-10-01 17:39:58 +0530

    Update Pattern Problems.c

--- Pattern Problems.c
+++ Pattern Problems.c
@@ -1,30 +1,29 @@
 //Pattern Programs
 
 #include<stdio.h>
-
 void main()
 {
 	int i, j, n;
 	printf("Enter the value of n: "); scanf("%d",&n);
-	
+
 	for(i=1; i<=n; i++)
 	{
 		for(j=0; j<i; j++)
 			printf("*");
 		printf("\n");
 	}
-	
+
 	printf("\n");
-	
+
 	for(i=1; i<=n; i++)
 	{
 		for(j=1; j<=i; j++)
 			printf("%d",j);
 		printf("\n");
 	}
-	
+
 	printf("\n");
-	
+
 	int cnt=1;
 	for(i=1; i<=n; i++)
 	{
@@ -35,9 +34,9 @@ void main()
 			}
 		printf("\n");
 	}
-	
+
 	printf("\n");
-	
+
 	for(i=1; i<=n; i++)
 	{
 		for(j=1; j<=i; j++)
@@ -45,4 +44,3 @@ void main()
 		printf("\n");
 	}	
 }
-
