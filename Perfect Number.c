1a10578830349fc987a17e33f900c6099fb66189
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-10-21 20:07:59 +0530

    Update Perfect Number.c

--- Perfect Number.c
+++ Perfect Number.c
@@ -2,14 +2,14 @@
 #include<stdio.h>
 int main()
 {
-	int number, i, sum=0;
+	int number, i, sum = 0;
 	printf("Enter the number: "); scanf("%d", &number);
 	
 	for(i=1; i<number; i++)
 		if(number % i==0)
 			sum+=i;
 	
-	if(number==sum)
+	if(number == sum)
 		printf("Perfect Number");
 	else
 		printf("Not a Perfect Number");
