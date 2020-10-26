a476fc9023ed03ce62ce437ff6b04858940df680
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-10-26 19:54:25 +0530

    Update Structure_Student.c

--- Structure_Student.c
+++ Structure_Student.c
@@ -11,9 +11,9 @@ struct student
 void main()
 {
 	int i, n;
-	
+
 	printf("Enter the number of studets: "); scanf("%d", &n);
-	
+
 	for(i=0; i<n; i++)
 	{
 		printf("Enter the name of the sutdent: "); fflush(stdin); gets(s[i].name);
@@ -21,9 +21,9 @@ void main()
 		printf("Enter the marks: "); scanf("%d", &s[i].marks);
 		printf("-------------\n");
 	}
-	
+
 	printf("\n-------------\n-------------\n\n");
-	
+
 	for(i=0; i<n; i++)
 	{
 		printf("The name is: "); puts(s[i].name);
