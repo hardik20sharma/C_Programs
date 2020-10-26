b4e03c83c4783bac8447456455048b20e68d50d4
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-10-26 19:57:45 +0530

    Update sizeof Int, Char, Float.c

--- sizeof Int, Char, Float.c
+++ sizeof Int, Char, Float.c
@@ -5,9 +5,9 @@ void main()
 	int a, a1, b1, c1;
 	float b;
 	char c;
-	a1=sizeof(a);
-	b1=sizeof(b);
-	c1=sizeof(c);
+	a1 = sizeof(a);
+	b1 = sizeof(b);
+	c1 = sizeof(c);
 	printf("\nSize of int: %d",a1);
 	printf("\nSize of float: %d",b1);
 	printf("\nSize of char: %d",c1);
